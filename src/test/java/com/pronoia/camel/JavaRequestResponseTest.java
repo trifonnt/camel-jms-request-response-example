4738338517b11a35b2b0ab3b207cbae835af0cca
Trifon Trifonov <trifont@gmail.com>
2016-12-06 22:34:31 +0200

    Minor code formatting.

--- src/test/java/com/pronoia/camel/JavaRequestResponseTest.java
+++ src/test/java/com/pronoia/camel/JavaRequestResponseTest.java
@@ -2,13 +2,10 @@ package com.pronoia.camel;
 
 import java.util.concurrent.TimeUnit;
 
-import com.pronoia.camel.stub.jms.RequestReplyServer;
-
 import org.apache.activemq.camel.component.ActiveMQComponent;
 import org.apache.activemq.junit.EmbeddedActiveMQBroker;
 import org.apache.camel.EndpointInject;
 import org.apache.camel.Exchange;
-import org.apache.camel.ExchangePattern;
 import org.apache.camel.Processor;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.component.mock.MockEndpoint;
@@ -18,96 +15,94 @@ import org.junit.After;
 import org.junit.Rule;
 import org.junit.Test;
 
+import com.pronoia.camel.stub.jms.RequestReplyServer;
+
 public class JavaRequestResponseTest extends CamelTestSupport {
-    @Rule
-    public EmbeddedActiveMQBroker broker = new EmbeddedActiveMQBroker();
+	@Rule
+	public EmbeddedActiveMQBroker broker = new EmbeddedActiveMQBroker();
 
-    final String requestResponseQueueName = "testQueue";
-    final boolean useMessageIDAsCorrelationID = true;
+	final String requestResponseQueueName = "testQueue";
+	final boolean useMessageIDAsCorrelationID = true;
 
-    @EndpointInject(uri = "mock://result")
-    MockEndpoint result;
+	@EndpointInject(uri = "mock://result")
+	MockEndpoint result;
 
-    RequestReplyServer requestReplyServerStub = new RequestReplyServer();
+	RequestReplyServer requestReplyServerStub = new RequestReplyServer();
 
-    Processor requestProcessor = new Processor() {
-        @Override
-        public void process(Exchange exchange) throws Exception {
-            String body = exchange.getIn().getBody(String.class);
-            exchange.getIn().setBody("Request: " + body);
+	Processor requestProcessor = new Processor() {
+		@Override
+		public void process(Exchange exchange) throws Exception {
+			String body = exchange.getIn().getBody(String.class);
+			exchange.getIn().setBody("Request: " + body);
 
-            return;
-        }
-    };
+			return;
+		}
+	};
 
-    Processor resultProcessor = new Processor() {
-        @Override
-        public void process(Exchange exchange) throws Exception {
-            String body = exchange.getIn().getBody(String.class);
-            if ( body.startsWith("Response") ) {
-                exchange.getIn().setBody( "We're good");
-            } else {
-                exchange.getIn().setBody( "Bad response");
-            }
-            return;
-        }
-    };
+	Processor resultProcessor = new Processor() {
+		@Override
+		public void process(Exchange exchange) throws Exception {
+			String body = exchange.getIn().getBody(String.class);
+			if (body.startsWith("Response")) {
+				exchange.getIn().setBody("We're good");
+			} else {
+				exchange.getIn().setBody("Bad response");
+			}
+			return;
+		}
+	};
 
-    @Override
-    protected void doPreSetup() throws Exception {
-        requestReplyServerStub.setConnectionFactory(broker.createConnectionFactory());
-        requestReplyServerStub.setRequestQueueName(requestResponseQueueName);
-        requestReplyServerStub.setUseMessageIDAsCorrelationID(true);
-        requestReplyServerStub.start();
-        super.doPreSetup();
-    }
+	@Override
+	protected void doPreSetup() throws Exception {
+		requestReplyServerStub.setConnectionFactory(broker.createConnectionFactory());
+		requestReplyServerStub.setRequestQueueName(requestResponseQueueName);
+		requestReplyServerStub.setUseMessageIDAsCorrelationID(true);
+		requestReplyServerStub.start();
+		super.doPreSetup();
+	}
 
-    @Override
-    @After
-    public void tearDown() throws Exception {
-        super.tearDown();
-        requestReplyServerStub.interrupt();
-        requestReplyServerStub.join(15000);
-    }
+	@Override
+	@After
+	public void tearDown() throws Exception {
+		super.tearDown();
+		requestReplyServerStub.interrupt();
+		requestReplyServerStub.join(15000);
+	}
 
-    @Override
-    protected JndiRegistry createRegistry() throws Exception {
-        JndiRegistry registry = super.createRegistry();
+	@Override
+	protected JndiRegistry createRegistry() throws Exception {
+		JndiRegistry registry = super.createRegistry();
 
-        ActiveMQComponent activeMQComponent = new ActiveMQComponent();
-        activeMQComponent.setBrokerURL( broker.getVmURL() );
+		ActiveMQComponent activeMQComponent = new ActiveMQComponent();
+		activeMQComponent.setBrokerURL(broker.getVmURL());
 
-        registry.bind( "test-broker", activeMQComponent);
+		registry.bind("test-broker", activeMQComponent);
 
-        return registry;
-    }
+		return registry;
+	}
 
-    @Override
-    protected RouteBuilder createRouteBuilder() throws Exception {
-        RouteBuilder builder = new RouteBuilder() {
-            @Override
-            public void configure() throws Exception {
-                from("file://data?fileName=some-file.txt&noop=true")
-                        .id( "file-consumer")
-                        .log("Processing ${file:name}")
-                        .process(requestProcessor)
-                        .log("Generated ${body}")
-                        .toF("test-broker://queue:%s?exchangePattern=InOut&useMessageIDAsCorrelationID=%b",
-                                requestResponseQueueName, useMessageIDAsCorrelationID)
-                        .process(resultProcessor)
-                        .to( "mock://result")
-                ;
-            }
-        };
+	@Override
+	protected RouteBuilder createRouteBuilder() throws Exception {
+		RouteBuilder builder = new RouteBuilder() {
+			@Override
+			public void configure() throws Exception {
+				from("file://data?fileName=some-file.txt&noop=true").id("file-consumer")
+					.log("Processing ${file:name}")
+					.process(requestProcessor)
+					.log("Generated ${body}")
+					.toF("test-broker://queue:%s?exchangePattern=InOut&useMessageIDAsCorrelationID=%b", requestResponseQueueName, useMessageIDAsCorrelationID)
+					.process(resultProcessor)
+					.to("mock://result");
+			}
+		};
 
-        return builder;
-    }
+		return builder;
+	}
 
-    @Test
-    public void testRoute() throws Exception {
-        result.expectedBodiesReceived("We're good");
+	@Test
+	public void testRoute() throws Exception {
+		result.expectedBodiesReceived("We're good");
 
-        assertMockEndpointsSatisfied(5, TimeUnit.SECONDS);
-    }
-
+		assertMockEndpointsSatisfied(5, TimeUnit.SECONDS);
+	}
 }
--- src/test/java/com/pronoia/camel/stub/jms/RequestReplyServer.java
+++ src/test/java/com/pronoia/camel/stub/jms/RequestReplyServer.java
@@ -14,142 +14,137 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class RequestReplyServer extends Thread {
-    Logger log = LoggerFactory.getLogger(this.getClass());
+	Logger log = LoggerFactory.getLogger(this.getClass());
 
-    ConnectionFactory connectionFactory;
-    String requestQueueName;
-    String userName;
-    String userPassword;
-    long receiveTimeout = 1000;
+	ConnectionFactory connectionFactory;
+	String requestQueueName;
+	String userName;
+	String userPassword;
+	long receiveTimeout = 1000;
 
-    boolean useMessageIDAsCorrelationID = false;
+	boolean useMessageIDAsCorrelationID = false;
 
-    // Internal members
-    Connection connection;
-    Session session;
-    MessageConsumer consumer;
+	// Internal members
+	Connection connection;
+	Session session;
+	MessageConsumer consumer;
 
-    @Override
-    public void start() {
-        try {
-            if (null == userName) {
-                connection = connectionFactory.createConnection();
-            } else {
-                connection = connectionFactory.createConnection(userName, userPassword);
-            }
+	@Override
+	public void start() {
+		try {
+			if (null == userName) {
+				connection = connectionFactory.createConnection();
+			} else {
+				connection = connectionFactory.createConnection(userName, userPassword);
+			}
 
-            final boolean transacted = false;
-            session = connection.createSession(transacted, Session.AUTO_ACKNOWLEDGE);
-            consumer = session.createConsumer(session.createQueue(requestQueueName));
-            connection.start();
-        } catch (Exception startEx) {
-            throw new RuntimeException( "Error in JMS startup", startEx);
-        }
+			final boolean transacted = false;
+			session = connection.createSession(transacted, Session.AUTO_ACKNOWLEDGE);
+			consumer = session.createConsumer(session.createQueue(requestQueueName));
+			connection.start();
+		} catch (Exception startEx) {
+			throw new RuntimeException("Error in JMS startup", startEx);
+		}
 
-        super.start();
-    }
+		super.start();
+	}
 
-    @Override
-    public void interrupt() {
-        super.interrupt();
+	@Override
+	public void interrupt() {
+		super.interrupt();
 
-        if (null != consumer) {
-            try {
-                consumer.close();
-            } catch (JMSException jmsEx) {
-                // Eat this
-            } finally {
-                consumer = null;
-            }
-        }
+		if (null != consumer) {
+			try {
+				consumer.close();
+			} catch (JMSException jmsEx) {
+				// Eat this
+			} finally {
+				consumer = null;
+			}
+		}
 
-        if (null != connection) {
-            try {
-                connection.close();
-            } catch (JMSException jmsEx) {
-                // Eat this
-            } finally {
-                connection = null;
-            }
-        }
+		if (null != connection) {
+			try {
+				connection.close();
+			} catch (JMSException jmsEx) {
+				// Eat this
+			} finally {
+				connection = null;
+			}
+		}
 
-        if (null != session) {
-            try {
-                session.close();
-            } catch (JMSException jmsEx) {
-                // Eat this
-            } finally {
-                session = null;
-            }
-        }
-    }
+		if (null != session) {
+			try {
+				session.close();
+			} catch (JMSException jmsEx) {
+				// Eat this
+			} finally {
+				session = null;
+			}
+		}
+	}
 
-    @Override
-    public void run() {
-        while (isAlive() && !isInterrupted()) {
-            try {
-                Message request = consumer.receive(receiveTimeout);
-                if (null == request) {
-                    log.info( "Waiting for message");
-                } else {
-                    TextMessage response = session.createTextMessage("Response Message");
-                    Destination replyTo = request.getJMSReplyTo();
-                    if (null != replyTo) {
-                        String correlationID = request.getJMSCorrelationID();
-                        if (null != correlationID) {
-                            response.setJMSCorrelationID(correlationID);
-                        } else if (useMessageIDAsCorrelationID) {
-                            response.setJMSCorrelationID( request.getJMSMessageID());
-                        }
+	@Override
+	public void run() {
+		while (isAlive() && !isInterrupted()) {
+			try {
+				Message request = consumer.receive(receiveTimeout);
+				if (null == request) {
+					log.info("Waiting for message");
+				} else {
+					TextMessage response = session.createTextMessage("Response Message");
+					Destination replyTo = request.getJMSReplyTo();
+					if (null != replyTo) {
+						String correlationID = request.getJMSCorrelationID();
+						if (null != correlationID) {
+							response.setJMSCorrelationID(correlationID);
+						} else if (useMessageIDAsCorrelationID) {
+							response.setJMSCorrelationID(request.getJMSMessageID());
+						}
 
-                        MessageProducer producer = session.createProducer(replyTo);
-                        producer.send(response);
-                        producer.close();
-                    }
-                }
-            } catch (JMSException jmsEx) {
+						MessageProducer producer = session.createProducer(replyTo);
+						producer.send(response);
+						producer.close();
+					}
+				}
+			} catch (JMSException jmsEx) {
 
-            }
-        }
-    }
+			}
+		}
+	}
 
-    public ConnectionFactory getConnectionFactory() {
-        return connectionFactory;
-    }
+	public ConnectionFactory getConnectionFactory() {
+		return connectionFactory;
+	}
+	public void setConnectionFactory(ConnectionFactory connectionFactory) {
+		this.connectionFactory = connectionFactory;
+	}
 
-    public void setConnectionFactory(ConnectionFactory connectionFactory) {
-        this.connectionFactory = connectionFactory;
-    }
+	public String getRequestQueueName() {
+		return requestQueueName;
+	}
+	public void setRequestQueueName(String requestQueueName) {
+		this.requestQueueName = requestQueueName;
+	}
 
-    public String getRequestQueueName() {
-        return requestQueueName;
-    }
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
 
-    public void setRequestQueueName(String requestQueueName) {
-        this.requestQueueName = requestQueueName;
-    }
+	public String getUserPassword() {
+		return userPassword;
+	}
+	public void setUserPassword(String userPassword) {
+		this.userPassword = userPassword;
+	}
 
-    public String getUserName() {
-        return userName;
-    }
-
-    public void setUserName(String userName) {
-        this.userName = userName;
-    }
-
-    public String getUserPassword() {
-        return userPassword;
-    }
-
-    public void setUserPassword(String userPassword) {
-        this.userPassword = userPassword;
-    }
-
-    public boolean isUseMessageIDAsCorrelationID() {
-        return useMessageIDAsCorrelationID;
-    }
-
-    public void setUseMessageIDAsCorrelationID(boolean useMessageIDAsCorrelationID) {
-        this.useMessageIDAsCorrelationID = useMessageIDAsCorrelationID;
-    }
+	public boolean isUseMessageIDAsCorrelationID() {
+		return useMessageIDAsCorrelationID;
+	}
+	public void setUseMessageIDAsCorrelationID(boolean useMessageIDAsCorrelationID) {
+		this.useMessageIDAsCorrelationID = useMessageIDAsCorrelationID;
+	}
 }
